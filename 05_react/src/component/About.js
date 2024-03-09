b8850953df4fc8a10cfa25b2848917ef5ab6aed3
Sivram Pradhan <programmersiv21@gmail.com>
2024-03-09 13:44:43 +0530

    React-5

---
+++ 05_react/src/component/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+
+const About = () => {
+
+    return (
+
+        <div className="bg-pink-500">
+            About Us
+        </div>
+
+    )
+}
+
+export default About
